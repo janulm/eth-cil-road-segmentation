831f2622103f1df13ff76bb4e2a4075089025b2f
Paul <paul.ellsiepen@gmail.com>
2024-06-18 13:29:09 +0200

    fixed scripts, excluded openstreet file clogging repo

---
+++ data/gmaps/rt-dataset-scripts/print_regions.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	// "rt_lib/lib"
+	"github.com/mitroadmaps/roadtracer/dataset/lib"
+	// "./lib"
+
+	"fmt"
+	"math"
+)
+
+const ZOOM = 18
+
+func main() {
+	widthWorld := 2 * math.Pi * 6378137 / math.Exp2(ZOOM) / 256 // meters per pixel
+	for _, region := range lib.GetRegions(widthWorld) {
+		fmt.Println(region)
+	}
+}
